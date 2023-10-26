a1b06cf7381180a65e1bec3a6380ade0563e14f2
Allan Kayan <4llankayan@gmail.com>
2023-10-26 10:01:52 -0300

    fix: remove unnecessary JWTAuth::getToken and add findOrFail

--- app/Http/Controllers/ShoppingListController.php
+++ app/Http/Controllers/ShoppingListController.php
@@ -9,6 +9,7 @@ use App\Http\Resources\ShoppingList\IndexResource;
 use App\Http\Resources\ShoppingList\ShowResource;
 use App\Models\Product;
 use App\Models\ShoppingList;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
 use Throwable;
 use Tymon\JWTAuth\Facades\JWTAuth;
@@ -16,8 +17,7 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 class ShoppingListController extends Controller
 {
     public function index() {
-        $token = JWTAuth::getToken();
-        $user = JWTAuth::toUser($token);
+        $user = auth()->user();
 
         $shopping_lists = ShoppingList::with('products')
             ->where('user_id', $user->id)
@@ -29,25 +29,18 @@ class ShoppingListController extends Controller
     public function store(StoreRequest $request) {
         $validated = $request->validated();
 
-        $token = JWTAuth::getToken();
-        $user = JWTAuth::toUser($token);
-
-        DB::beginTransaction();
+        $user = auth()->user();
 
         try {
             $shopping_list = ShoppingList::create([
                 'name' => $validated['name'],
                 'user_id' => $user->id,
             ]);
-
-            DB::commit();
         } catch (Throwable $th) {
-            DB::rollBack();
-
             return response()->json([
                 'status' => 'failed',
                 'message' => $th->getMessage(),
-            ]);
+            ], 500);
         }
 
         return response()->json([
@@ -58,21 +51,13 @@ class ShoppingListController extends Controller
     }
 
     public function show($id) {
-        $token = JWTAuth::getToken();
-        $user = JWTAuth::toUser($token);
+        $user = auth()->user();
 
         $shopping_list = ShoppingList::with('products')
-            ->find($id);
+            ->findOrFail($id);
 
         if ($shopping_list->user != $user) {
             return response()->json(['error' => 'You do not have permission to access this shopping list'], 403);
-        }
-
-        if (is_null($shopping_list)) {
-            return response()->json([
-                'status' => 'failed',
-                'message' => 'Shopping List not found',
-            ], 404);
         }
 
         return app(ShowResource::class, ['resource' => $shopping_list]);
@@ -81,92 +66,73 @@ class ShoppingListController extends Controller
     public function addProduct($id, AddProductRequest $request) {
         $validated = $request->validated();
 
-        $shopping_list = ShoppingList::with('products')->find($id);
-        $product = Product::find($validated['product_id']);
+        $shopping_list = ShoppingList::with('products')->findOrFail($id);
+        $product = Product::findOrFail($validated['product_id']);
 
-        $token = JWTAuth::getToken();
-        $user = JWTAuth::toUser($token);
+        $user = auth()->user();
 
         if ($shopping_list->user != $user) {
-            return response()->json(['error' => 'You do not have permission to add products this shopping list'], 403);
+            return response()->json(['message' => 'You do not have permission to add products this shopping list'], 403);
         }
 
         if ($shopping_list->closed) {
-            return response()->json(['error' => 'This shopping list is closed'], 400);
+            return response()->json(['message' => 'This shopping list is closed'], 400);
         }
 
         if ($shopping_list->products->contains($product)) {
-            return response()->json(['error' => 'This product is already on the shopping list'], 400);
+            return response()->json(['message' => 'This product is already on the shopping list'], 400);
         }
-
-        DB::beginTransaction();
 
         try {
             $shopping_list->products()->syncWithoutDetaching($product);
-
-            DB::commit();
         } catch (Throwable $th) {
-            DB::rollBack();
-
             return response()->json([
-                'status' => 'failed',
                 'message' => $th->getMessage(),
-            ]);
+            ], 500);
         }
 
         return response()->json([
-            'status' => 'success',
             'message' => 'Product successfully added to the shopping list',
         ]);
     }
 
-    public function removeProduct($id, RemoveProductRequest $request) {
+    public function removeProduct($id, RemoveProductRequest $request): JsonResponse {
         $validated = $request->validated();
 
-        $shopping_list = ShoppingList::with('products')->find($id);
-        $product = Product::find($validated['product_id']);
+        $shoppingList = ShoppingList::with('products')->findOrFail($id);
+        $product = Product::findOrFail($validated['product_id']);
 
-        $token = JWTAuth::getToken();
-        $user = JWTAuth::toUser($token);
+        $user = auth()->user();
 
-        if ($shopping_list->user != $user) {
-            return response()->json(['error' => 'You do not have permission to remove products from this shopping list'], 403);
+        if ($shoppingList->user != $user) {
+            return response()->json(['message' => 'You do not have permission to remove products from this shopping list'], 403);
         }
 
-        if ($shopping_list->closed) {
-            return response()->json(['error' => 'This shopping list is closed'], 400);
+        if ($shoppingList->closed) {
+            return response()->json(['message' => 'This shopping list is closed'], 400);
         }
 
-        if (!$shopping_list->products->contains($product)) {
-            return response()->json(['error' => "This product isn't on the shopping list"], 400);
+        if (!$shoppingList->products->contains($product)) {
+            return response()->json(['message' => "This product isn't on the shopping list"], 400);
         }
-
-        DB::beginTransaction();
 
         try {
-            $shopping_list->products()->detach($product);
-
-            DB::commit();
+            $shoppingList->products()->detach($product);
         } catch (Throwable $th) {
-            DB::rollBack();
-
             return response()->json([
-                'status' => 'failed',
                 'message' => $th->getMessage(),
-            ]);
+            ], 500);
         }
 
         return response()->json([
-            'status' => 'success',
             'message' => 'Product successfully removed from shopping list',
         ]);
     }
 
-    public function checkout($id) {
+    public function checkout($id): JsonResponse {
         $shopping_list = ShoppingList::with('products')->find($id);
 
-        $token = JWTAuth::getToken();
-        $user = JWTAuth::toUser($token);
+        $user = auth()->user();
 
         if ($shopping_list->user != $user) {
             return response()->json(['error' => 'You do not have permission to close this shopping list'], 403);
@@ -176,26 +142,18 @@ class ShoppingListController extends Controller
             return response()->json(['error' => 'This shopping list is already closed'], 400);
         }
 
-        DB::beginTransaction();
-
         try {
             $shopping_list->update([
                 'closed' => true,
             ]);
-
-            DB::commit();
         } catch (Throwable $th) {
-            DB::rollBack();
-
             return response()->json([
-                'status' => 'failed',
-                'message' => $th->getMessage(),
-            ]);
+                'message' => $th->getMessage()
+            ], 500);
         }
 
         return response()->json([
-            'status' => 'success',
-            'message' => 'Shopping List successfully closed',
+            'message' => 'Shopping List successfully closed'
         ]);
     }
 }
